806dc5dee50331b81d9004d377f80642a67d225d
Saysamiche <140890694+Saysamiche@users.noreply.github.com>
2024-02-21 09:45:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,34 +2,38 @@
 
 const getValueOne = function () {
   if (document.querySelector(".one").textContent === "1") {
-    document.querySelector(".display").textContent = "1";
-  } else {
-    document.querySelector(".display").textContent = "";
+    document.querySelector(".display").textContent += "1";
   }
+  // else {
+  //   document.querySelector(".display").textContent = "";
+  // }
 };
 
 const getValueTwo = function () {
   if (document.querySelector(".two").textContent === "2") {
-    document.querySelector(".display").textContent = "2";
-  } else {
-    document.querySelector(".display").textContent = "";
+    document.querySelector(".display").textContent += "2";
   }
+  //   else {
+  //   document.querySelector(".display").textContent = "";
+  // }
 };
 
 const getValueThree = function () {
   if (document.querySelector(".three").textContent === "3") {
-    document.querySelector(".display").textContent = "3";
-  } else {
-    document.querySelector(".display").textContent = "";
+    document.querySelector(".display").textContent += "3";
   }
+  //   else {
+  //   document.querySelector(".display").textContent = "";
+  // }
 };
 
 const getValueFour = function () {
   if (document.querySelector(".four").textContent === "4") {
-    document.querySelector(".display").textContent = "4";
-  } else {
-    document.querySelector(".display").textContent = "";
+    document.querySelector(".display").textContent += "4";
   }
+  //   else {
+  //   document.querySelector(".display").textContent = "";
+  // }
 };
 
 document.querySelector(".one").addEventListener("click", getValueOne);
